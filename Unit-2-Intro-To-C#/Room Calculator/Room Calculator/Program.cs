2e35716cc6a1be057061b458c409e830d11fdd26
Dipo owoiya <dipocareers@gmail.com>
2025-04-16 12:08:49 -0400

    "mooooo"

---
+++ Unit-2-Intro-To-C#/Room Calculator/Room Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Room_Calculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Room Calculator, Echo!");
+        Console.WriteLine("Please enter a number:");
+        double number1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please enter a second number:");
+        double number2 = Convert.ToInt32(Console.ReadLine());
+        //double number =  number + .5;
+        Console.WriteLine("Result: " + number1 + number2);
+        
+        
+    }
+    
+}
---
+++ Unit-3-Collections/Day-1-Intro-to-Collections-Class/CowJumpProject/CowJumpProject/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CowJumpProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome fellow Tester,Please Enter a Sentence");
+        string sentence = Console.ReadLine();
+        string[] words = sentence.Split(" ");
+
+        foreach (string word in words) 
+        {
+            Console.WriteLine(word);
+        }
+        
+        Console.WriteLine("Would you like to continue? (y/n)");
+        while (Console.ReadLine() != "y")
+        {
+           Console.WriteLine("Would you like to continue? (y/n)");
+        }
+        
+    }
+    
+}
