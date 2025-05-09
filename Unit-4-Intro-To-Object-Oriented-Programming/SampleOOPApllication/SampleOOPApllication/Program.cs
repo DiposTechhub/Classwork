fe618ee728b1d06efd5d204d197507249f7e647e
Dipo owoiya <dipocareers@gmail.com>
2025-05-08 22:04:45 -0400

    yes

---
+++ Unit-4-Intro-To-Object-Oriented-Programming/SampleOOPApllication/SampleOOPApllication/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SampleOOPApllication;
+
+class Program
+{
+    //Application Program will define and manipulate objects
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World CUP Drunk!");
+        Person aPerson = new Person("Denzel", "95", "243.5", "50", "Male", "Green");
+        Console.WriteLine(aPerson);
+    }
+}
